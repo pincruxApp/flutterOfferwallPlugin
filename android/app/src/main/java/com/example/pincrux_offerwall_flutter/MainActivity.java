26908876815acac970a69c6253a00a6f88eb9b1f
melo <melo@pincrux.com>
2025-09-04 13:30:52 +0900

    [Android] MainActivity.java - setOfferwallCategory()에서 Int -> Enum으로 호출 방식 변경

--- android/app/src/main/java/com/example/pincrux_offerwall_flutter/MainActivity.java
+++ android/app/src/main/java/com/example/pincrux_offerwall_flutter/MainActivity.java
@@ -109,7 +109,24 @@ public class MainActivity extends FlutterActivity {
 
                         case "setOfferwallCategory": {
                             int category = call.argument("category");
-                            offerwall.setOfferwallCategory(category);
+                            switch (category) {
+                                case 2:
+                                    offerwall.setOfferwallCategory(PincruxOfferwall.CATEGORY_CPA);
+                                    break;
+                                case 3:
+                                    offerwall.setOfferwallCategory(PincruxOfferwall.CATEGORY_SOCIAL);
+                                    break;
+                                case 4:
+                                    offerwall.setOfferwallCategory(PincruxOfferwall.CATEGORY_CPS);
+                                    break;
+                                case 5:
+                                    offerwall.setOfferwallCategory(PincruxOfferwall.CATEGORY_GAME);
+                                    break;
+                                case 1:
+                                default:
+                                    offerwall.setOfferwallCategory(PincruxOfferwall.CATEGORY_FINANCE);
+                                    break;
+                            }
                             break;
                         }
 
