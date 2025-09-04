f5484e8997768156023182d3507e7169ab28bffa
melo <melo@pincrux.com>
2025-09-04 13:59:52 +0900

    [iOS] AppDelegate.swift - setOfferwallCategory() 기본 호출 값을 .Finance로 설정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -134,8 +134,6 @@ import PincruxOfferwall
                    let args = call.arguments as? Dictionary<String, Any>,
                    let category = args["category"] as? Int {
                     switch category {
-                    case 1 :
-                        self.offerwall?.setOfferwallCategory(.Finance)
                     case 2 :
                         self.offerwall?.setOfferwallCategory(.CPA)
                     case 3 :
@@ -144,8 +142,10 @@ import PincruxOfferwall
                         self.offerwall?.setOfferwallCategory(.CPS)
                     case 5 :
                         self.offerwall?.setOfferwallCategory(.Game)
-                    default :
-                        break
+                    case 1:
+                        fallthrough
+                    default:
+                        self.offerwall?.setOfferwallCategory(.Finance)
                     }
                 }
 
